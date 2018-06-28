ca6f78c062137870dbb8d696ffdc579fe4bd0d23
tusmester <tusmester@gmail.com>
2018-06-28 17:43:55 +0200

    Netstandard/framework461 (#5)
    * Raise .net framework version to 461
    * Raise Services reference to 7.2
    * Add component class and raise version to 7.2

---
+++ src/Notification/NotificationComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SenseNet.ContentRepository;
+
+namespace SenseNet.Notification
+{
+    public class NotificationComponent : SnComponent
+    {
+        public override string ComponentId => "SenseNet.Notification";
+        public override Version SupportedVersion { get; } = new Version(7, 1, 0);
+    }
+}
